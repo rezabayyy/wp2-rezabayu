528197b634aad03901784a0a6998ccb72b219919
rezabayyy <rezabayu2810@gmail.com>
2023-09-26 16:26:49 +0700

    menambahkan daftar pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya bayu
+        Saya tingga di daerah ujung kulon
+        olahraga yang saya sukai adalah
+        balap siput";
+     }
+}
